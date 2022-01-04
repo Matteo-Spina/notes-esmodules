3ce2f77409ef38d0bc6028442208693209c148cd
Matteo <matteo.spina@hotmail.com>
2022-01-04 18:51:00 +0100

    try something

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 // main.js
 // this is a js module (> es6)
+console.log("import main");
 import importedData from "./modules/doFetch.js";
 
+console.log("start main after import");
 const target = document.getElementById("target");
 target.innerText = importedData.ok ?? importedData.notOk;
 console.log("end main");
