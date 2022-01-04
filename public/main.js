051decfce5d3a658b23c387eceba945525eac33b
Matteo <matteo.spina@hotmail.com>
2022-01-04 18:39:38 +0100

    import a default use another name

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 // main.js
 // this is a js module (> es6)
-import { dataToExport } from "./modules/doFetch.js";
+import importedData from "./modules/doFetch.js";
 
 const target = document.getElementById("target");
-target.innerText = dataToExport.ok ?? dataToExport.notOk;
+target.innerText = importedData.ok ?? importedData.notOk;
 console.log("end main");
