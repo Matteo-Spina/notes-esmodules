8a25e15e217f2c83dd1187add0596743e54e55f4
Matteo <matteo.spina@hotmail.com>
2022-01-04 18:37:58 +0100

    separate modules

--- public/main.js
+++ public/main.js
@@ -1,36 +1,7 @@
 // main.js
 // this is a js module (> es6)
+import { dataToExport } from "./modules/doFetch.js";
 
 const target = document.getElementById("target");
-
-async function fetchData() {
-  // check for a successful fecth, can throws error
-  const response = await fetch("/api/hello"); //
-  // check response is within 200-299
-  if (!response.ok) {
-    throw new Error("HTTP error, status = " + response.status);
-  }
-  // check some headers
-  const contentType = response.headers.get("content-type");
-  if (!contentType || !contentType.includes("text/plain")) {
-    throw new TypeError("content-type is not text/plain");
-  }
-  return response.text();
-}
-
-async function renderData(data) {
-  target.innerText = data.ok ?? data.notOk;
-}
-
-async function main() {
-  let data = {};
-  try {
-    data.ok = await fetchData();
-  } catch (error) {
-    data.notOk = error;
-    console.log(error);
-  }
-  await renderData(data);
-}
-
-main();
+target.innerText = dataToExport.ok ?? dataToExport.notOk;
+console.log("end main");
---
+++ public/modules/doFetch.js
@@ -0,0 +1,29 @@
+// doFetch.js
+// js module (es > 6)
+let dataToExport;
+async function fetchData() {
+  // check for a successful fecth, can throws error
+  const data = {};
+  const response = await fetch("/api/hello"); //
+  // check response is within 200-299
+  if (!response.ok) {
+    data.notOk = `response status: ${response.status}`;
+    return;
+  }
+  // check some headers
+  // data.contentType = response.headers.get("content-type");
+  // if (!contentType || !contentType.includes("text/plain")) {
+  //   data.notOk = `Content-Type is not `;
+  // }
+  // parse response
+  const parsedRes = await response.text();
+  data.ok = parsedRes;
+  return data;
+}
+try {
+  dataToExport = await fetchData();
+} catch (error) {
+  console.log("something went wrong");
+}
+console.log("end fetch");
+export default dataToExport;
