fd6e62f3e49520a4d2cddbca16a80354541dae90
Matteo <matteo.spina@hotmail.com>
2022-01-05 17:33:56 +0100

    add boxes

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 // main.js
 // this is a js module (> es6)
-console.log("import main");
+
+// console.log("logged after import");
 import importedData from "./modules/doFetch.js";
 
-console.log("start main after import");
-const target = document.getElementById("target");
+const moduleA = document.getElementById("module-a");
+const target = moduleA.lastElementChild;
 target.innerText = importedData.ok ?? importedData.notOk;
-console.log("end main");
